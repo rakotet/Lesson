031277755859f90eb3eaf380a9f7c079302ffdb9
rakot <rakotet@gmail.com>
2020-05-06 19:27:30 +0400

    Немного свойст и методов строк в js

--- js/script8.js
+++ js/script8.js
@@ -3,6 +3,12 @@ var str2 = 'Hello World 2'
 
 var personName = 'Виктор'
 var message = 'Человека зовут "' + personName + '"'
+var message2 = 'Человека зовут \'' + personName + '\'' // обратный слешь \ экранирует следуший символ после себя
 
 console.log(str1)
 console.log(message)
+console.log(message2)
+
+var newMessage = "Hello World!!!!!!"
+
+console.log(newMessage.length) // Метод объекта (в js всё объект, даже строка) возвращающий длину строки. 
