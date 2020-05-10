026467fe0bd647c4ec4d96e61e3a4cbed64e461c
rakotet <rakotet@gmail.com>
2020-05-10 19:14:36 +0400

    Урок JS про строки и их методы

--- js/script8.js
+++ js/script8.js
@@ -12,3 +12,10 @@ console.log(message2)
 var newMessage = "Hello World!!!!!!"
 
 console.log(newMessage.length) // Метод объекта (в js всё объект, даже строка) возвращающий длину строки. 
+console.log(newMessage.toUpperCase()) //Метод переводит все символы в верхний регистр
+console.log(newMessage.toLowerCase()) //Метод переводит все символы в нижний регистр
+console.log(newMessage.charAt(1)) // Возвращает символ номер которого указан в аргументе метода (нумерация идет с 0)
+console.log(newMessage.indexOf('World')) // Возвращает номер символа с которого начинается аргумент метода, если такого нет, вернёт -1
+console.log(newMessage.substr(1, 4)) // Первый аргумент номер символа с которого начинаем, второй которым заканчиваем, символы между ними возвращаем.
+console.log(newMessage.substr(newMessage.indexOf('World'), 5)) // Вернет World
+console.log(newMessage.substring(1)) // Аргумент номер символа с которого начинаем, и возвращаем всю строку начиная от него
