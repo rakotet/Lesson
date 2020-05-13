0355300cdba720ecccf7e90524ab394076ec114b
rakot <rakotet@gmail.com>
2020-05-13 19:28:11 +0400

    Еще уроки по js

--- js/script14.js
+++ js/script14.js
@@ -6,3 +6,13 @@ a.setAttribute('title', 'Go Yandex')
 a.textContent = 'Yandex' // меняем наполнение внутри тега <a></a>
 
 console.log()
+
+//Классы
+
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+
+box1.classList.add('red') //Добавляем еще один класс в влок div
+box2.classList.remove('blue')//Удаляем указанный класс и з блока div
+var hasClass = box2.classList.contains('box')//Проверяем содержит ли блок указанынй класс
+console.log(hasClass)//Получим True т.к. этот класс есть в блоке
