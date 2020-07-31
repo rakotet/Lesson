e4a59a6bd3517af1ae63363c4af62a158890d9a5
rakot <rakotet@gmail.com>
2020-07-31 16:49:23 +0400

    php89++++

--- php/domains/mysyte.local/index.php
+++ php/domains/mysyte.local/index.php
@@ -1,1 +1,12 @@
 <?php
+if (isset($_POST['upload']))
+    print_r($_FILES);
+?>
+<form name="upload" action="index.php" method="post" enctype="multipart/form-data">
+    <p>
+        <input type="file" name="im">
+    </p>
+    <p>
+        <input type="submit" name="upload" value="Отправить">
+    </p>
+</form>
