4e5d44ace6e3b25af8ec5e174cfac614a57cf7ba
rakotet <rakotet@gmail.com>
2020-08-09 14:09:47 +0400

    stabilno avtoriz

--- php/domains/reporting34/core/route_class.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (session_status() === 2) {
-    include '../view/main_view.php';
-}
-
--- php/domains/reporting34/core/session_class.php
+++ php/domains/reporting34/core/session_class.php
@@ -1,21 +1,21 @@
 <?php
 require_once 'bd_authorization_class.php';
 
-session_start();
-
 $error = false; // устанавливаем флаг ошибки
 if (isset($_POST['auth'])) // проверяем была ли переданна форма
 {
+    session_start();
+
+    if (isset($_GET['f']) && $_GET['f'] == 'logout') // проверяем есть ли GET запрос f и если он равен logout, то удаляем сесси логина и пароля
+    {
+        unset($_SESSION['login']);
+        unset($_SESSION['password']);
+    }
     $_SESSION['login'] = $_POST['login']; // открываем сессию записываем полученные из POST запроса переменные в сессию
     $_SESSION['password'] = md5($_POST['password']);
     $error = true;
 }
 
-if (isset($_GET['f']) && $_GET['f'] == 'logout') // проверяем есть ли GET запрос f и если он равен logout, то удаляем сесси логина и пароля
-{
-    unset($_SESSION['login']);
-    unset($_SESSION['password']);
-}
 
 $iss = isset($_SESSION['login']) && isset($_SESSION['password']);
 
