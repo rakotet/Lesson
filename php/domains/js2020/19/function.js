2258b1fc91b361ca9036229e1da20a1d18eb0b69
rakot <rakotet@gmail.com>
2020-09-08 15:36:55 +0400

    lesson 22 js

---
+++ php/domains/js2020/19/function.js
@@ -0,0 +1,17 @@
+const C = 10;
+
+function sum(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
