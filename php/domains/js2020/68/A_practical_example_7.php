102a846d47a8861e121f090781d67d332813c32f
rakot <rakotet@gmail.com>
2020-10-12 19:49:55 +0400

    js 68++

--- php/domains/js2020/68/A_practical_example_7.php
+++ php/domains/js2020/68/A_practical_example_7.php
@@ -1,1 +1,13 @@
 <?php
+    if (isset($_POST['text'])) {
+        $text = htmlspecialchars($_POST['text']);
+        if (mb_strlen($text) == 0) echo 'Ќет сообщени€';
+        else {
+            file_put_contents('chat.txt', file_get_contents('chat.txt') . "\n" . $text); // записываем сообщение из формы в файл
+            echo 'ok';
+        }
+    }
+    else {
+        $arr = explode("\n", file_get_contents('chat.txt')); // ф-€ с помошью разделител€ строки из файла преобразует в массив
+        echo json_encode($arr); // кодируем массив в json и передаЄм на выход€щий поток
+    }
