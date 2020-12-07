2f0f77bff7a8f06bd180c0708a609b40a33a1375
rakotet <rakotet@gmail.com>
2020-12-07 20:32:25 +0400

    js plagin ok!

--- js/script34.js
+++ js/script34.js
@@ -6,9 +6,28 @@ class Dropdown { // создаем класс
 
         this.$el.addEventListener('click', event => {
             if(event.target.classList.contains('dropdown__label')) { // если кликнули по элементу в котором есть класс dropdown__label
+                if(this.$el.classList.contains('open')) { // если класс open есть в элементе то вызвать close если нет, вызвать open
+                    this.close();
+                } else {
+                    this.open();
+                }
+            } else if(event.target.tagName.toLowerCase() === 'li') { // если мы сделали клик на тег <li>
+                this.select(event.target.dataset.id); // передаем в метод значение data-* (в нашем случае * = id)
 
             }
         });
+
+        const itemsHTML = this.items.map(i => { // вызываем map у массива items и спомощью стрелочной ф-и 
+            return `<li data-id="${i.id}">${i.label}</li>` // преобразуем каждый элемент в HTML код 
+        }).join(' '); // преобразует элементы массива в строки и разделяет их символом указанным в аргументе метода
+
+        this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML); // вставляем массив из HTML блоков <li> внутрь <ul>
+    }
+
+    select(id) {
+        const item = this.items.find(i => i.id === id); // записываем в переменную объект, из массива items у которого id совпадает c переданным id в ф-ю
+        this.$el.querySelector('.dropdown__label').textContent = item.label; // меняем содержание элемента на свойство label объекта item
+        this.close();
     }
 
     open() {
