c236067584a21020a72566fe0302ed48161d32f4
rakot <rakotet@gmail.com>
2020-12-14 19:54:51 +0400

    js 51+++

--- js/script41.js
+++ js/script41.js
@@ -9,30 +9,76 @@
 // promise
 //     .then(num => num *= 2)
 //     .catch(err => console.log(err))
-//     .then(num => numm *= 3)
+//     .then(num => num *= 3)
 //     .finally(() => console.log('fimally'))
 
 // Реализуем свой класс с таким же функционалом как у класса промис
 
 class MyPromise {
     constructor(callback) {
+        this.onCatch = null
+        this.onFinally = null
+        this.thenCbs = []
+        this.isRejected = false
 
-        function resolver() {}
+        function resolver(data) {
+            if(this.isRejected) {
+                return
+            }
 
-        function rejecter() {}
+            this.thenCbs.forEach(cb => {
+                data = cb(data)
+            })
+            
+            if(this.onFinally) {
+                this.onFinally()
+            }
+        }
 
-        callback(resolver, rejecter)
+        function rejecter(error) {
+            this.isRejected = true
+            if(this.onCatch) {
+                this.onCatch(error)
+            }
+
+            if(this.onFinally) {
+                this.onFinally()
+            }
+        }
+
+        callback(resolver.bind(this), rejecter.bind(this))
     }
 
     then(cb) {
-
+        this.thenCbs.push(cb)
+        return this
     }
 
     catch(cb) {
+        this.onCatch = cb
+        return this
 
     }
 
     finally(cb) {
+        this.onFinally = cb
+        return this
 
     }
 }
+
+
+
+const promise = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        reject('Some error')
+        resolve(2)
+    }, 2000)
+})
+
+promise
+    .then(num => num *= 2)
+    .catch(err => console.error(err))
+    .then(num => num *= 3)
+    .finally(() => console.log('fimally'))
+    .then(num => console.log('Done!', num))
