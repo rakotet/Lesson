10a2a041e1f4b8a79eb91b10871e2be448b7262a
rakot <rakotet@gmail.com>
2020-12-22 19:43:40 +0400

    wp 8+

--- webpack-lesson/modules/header.component.js
+++ webpack-lesson/modules/header.component.js
@@ -5,6 +5,6 @@ $('<h1/>')
     .text('Hello world from JQuery')
     .css({
         textAlign: 'center',
-        color: 'red'
+        color: 'blue'
     })
     .appendTo($('header'))
--- webpack-lesson/webpack.config.js
+++ webpack-lesson/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path') // подключием модуль nojs для работы с путями папок и файлов
 const HTMLPlugin = require('html-webpack-plugin') // подключаем модуль(плагин) webpack для создания html страниц
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключаем плагин для работы с css файлами в отдельном файле
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin') // подключаем плагин для оптимизации css файлов
+const TerserPlugin = require("terser-webpack-plugin") // подключаем плагин для оптимизации js файлов
 
 
 module.exports = { // экспортируем модуль наружу
@@ -9,18 +12,42 @@ module.exports = { // экспортируем модуль наружу
         filename: 'bundle.js', // название файла в который будет идти сборка
         path: path.resolve(__dirname, 'dist') // путь конечного файла, 1 путь до текущего файла, 2 название папки для конечного файла
     },
-    plugins: [ // инициализируем плагины в вебпаке (просто создаём экземпляр класса плагина который хотим подключить и передаём в его конствуктор свойства в виде объекта)
+    optimization: { // подключаем свойства плагина для оптимизации css и js файлов
+        minimize: true,
+        minimizer: [
+            new OptimizeCssAssetsPlugin({}),
+            new TerserPlugin()
+        ]
+    },
+    devServer: { // настройка devServer
+        contentBase: path.resolve(__dirname, 'dist'), // конечная папка проэкта
+        port: 4200 // на этом порту будет запущен наш локальный сервер
+    },
+    plugins: [ // инициализируем плагины в вебпаке (просто создаём экземпляр класса плагина который хотим подключить и передаём в его конструктор свойства в виде объекта)
         new HTMLPlugin({
             filename: 'index.html', // имя файла который сгенерирует webpack в папку dist
             template: './src/index.html' // указываем шаблон html файла который будет взят за основу нашего проэкта
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css' // выходной файл css
         })
     ],
-    module: { // подключаем css-loader (свойства и их описание смотреть на офф сайте)
+    module: { // подключаем css-loader и mini-css-extract-plugin (свойства и их описание смотреть на офф сайте)
         rules: [
             {
                 test: /\.css$/, // регулярное выражение которым мы определяем расширение файла (в нашем случае .css)
-                use: ['style-loader', 'css-loader'] // чем обрабатывать
-            }
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] // чем обрабатывать, 1 приминять наши файлы css к проэкту, 2 загружать css файлы
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
         ]
     }
 }
