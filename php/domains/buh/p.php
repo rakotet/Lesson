77943c1ac6049ae67a3c24df793ed32f481695c4
rakotet <rakotet@gmail.com>
2021-02-05 23:47:55 +0300

    pitays

--- php/domains/buh/p.php
+++ php/domains/buh/p.php
@@ -6,7 +6,7 @@ $result1 = [];
 $user2 = file('2user.txt');
 $num2 = file('2num.txt');
 $result2 = [];
-$result3 = [];
+$result3 = '';
 
 for ($i = 0; $i < count($user1); $i++) {
     $result1[$i] = [$user1[$i], $num1[$i]] ;
@@ -16,13 +16,20 @@ for ($i = 0; $i < count($user2); $i++) {
     $result2[$i] = [$user2[$i], $num2[$i]];
 }
 
-// for ($i = 0; $i < count($result1); $i++) {
-//     $z = $result1[$i][$i];
-//     for ($ii = 0; $ii < count($result2); $ii++) {
-//         if($z == $result2[$ii][$ii]) {
+for ($i = 0; $i < count($result1); $i++) {
+    $x = 0;
+    for ($ii = 0; $ii < count($result2); $ii++) {
+        $y = 0;
+        if($result1[$i][$x] == $result2[$ii][$y]) {
+            $result3 = $result1[$i][$x];
+            if($result1[$i][$x + 1] == $result2[$ii][$y + 1]){
+                $result3 = $result3.' - '.'yes'."\n";
+            } else {
+                $result3 = $result3.' '.$result1[$i][$x + 1].' '. $result2[$ii][$y + 1]."\n";
+            }
+        }
+    }
+    
+}
 
-//         }
-//     }
-// }
-
-print_r($result2);
+echo $result3;
