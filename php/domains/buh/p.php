ce4c2b44c100bf39466e09e7b9823bb569e9058b
rakotet <rakotet@gmail.com>
2021-02-06 18:41:46 +0300

    buh yes!

--- php/domains/buh/p.php
+++ php/domains/buh/p.php
@@ -6,7 +6,7 @@ $result1 = [];
 $user2 = file('2user.txt');
 $num2 = file('2num.txt');
 $result2 = [];
-$result3 = '';
+$result3 = '123';
 
 for ($i = 0; $i < count($user1); $i++) {
     $result1[$i] = [$user1[$i], $num1[$i]] ;
@@ -17,19 +17,28 @@ for ($i = 0; $i < count($user2); $i++) {
 }
 
 for ($i = 0; $i < count($result1); $i++) {
-    $x = 0;
+    
     for ($ii = 0; $ii < count($result2); $ii++) {
-        $y = 0;
-        if($result1[$i][$x] == $result2[$ii][$y]) {
-            $result3 = $result1[$i][$x];
-            if($result1[$i][$x + 1] == $result2[$ii][$y + 1]){
+       
+        if(mb_strtolower(trim($result1[$i][0])) == mb_strtolower(trim($result2[$ii][0]))) {
+            $result3 = $result1[$i][0];
+            if(mb_strtolower(trim($result1[$i][1])) == mb_strtolower(trim($result2[$ii][1]))) {
                 $result3 = $result3.' - '.'yes'."\n";
             } else {
-                $result3 = $result3.' '.$result1[$i][$x + 1].' '. $result2[$ii][$y + 1]."\n";
+                $result3 = $result3.' '.$result1[$i][1].' '. $result2[$ii][1]."\n";
             }
         }
     }
     
 }
 
-echo $result3;
+echo '_____________________________________________'."<br/>";
+
+echo $result3."<br/>";
+$a = $result1[0][0];
+$b = $result2[0][0];
+if($a == $b) {
+    echo 'yes';
+} else {
+    echo $result1[0][0].' - '.$result2[0][0];
+}
--- php/domains/myServerPhpTest/server.php
+++ php/domains/myServerPhpTest/server.php
@@ -6,14 +6,33 @@ use Workerman\Lib\Timer;
 use Workerman\Worker;
 
 
-$worker = new Worker("websocket://192.168.0.12:8001");
+$worker = new Worker("websocket://localhost:8001");
 
 $worker->onConnect = function($connection) use ($worker) {
     echo "Hello World!\n";
     echo $connection->id."\n";
     $connection->userName = 'anonim';
+
+    $user1 = file('1user.txt');
+    $num1 = file('1num.txt');
+    $result1 = [];
+    $user2 = file('2user.txt');
+    $num2 = file('2num.txt');
+    $result2 = [];
+    $result3 = [];
+
+    for ($i = 0; $i < count($user1); $i++) {
+        $result1[$i] = [$user1[$i], $num1[$i]] ;
+    }
+
+    for ($i = 0; $i < count($user2); $i++) {
+        $result2[$i] = [$user2[$i], $num2[$i]];
+    }
     
-    
+    $result3[0] = $result1;
+    $result3[1] = $result2;
+
+    $connection->send(json_encode($result3));
     // foreach ($worker->connections as $c) {
     //     $c->send($c->id);
     // }
