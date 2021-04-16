a7fdf2c6975a933fdfd77833c53d1fbaebc2c218
rakot <rakotet@gmail.com>
2021-04-16 19:24:12 +0300

    audio

---
+++ php/domains/voice/voice.js
@@ -0,0 +1,61 @@
+const URL = 'voice.php';
+let div = document.createElement('div');
+div.id = 'messages';
+div.innerText = '111';
+let start = document.createElement('button');
+start.id = 'start';
+start.innerHTML = 'Start';
+let stop = document.createElement('button');
+stop.id = 'stop';
+stop.innerHTML = 'Stop';
+document.body.appendChild(div);
+document.body.appendChild(start);
+document.body.appendChild(stop);
+navigator.mediaDevices.getUserMedia({ audio: true})
+    .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+
+        document.querySelector('#start').addEventListener('click', function(){
+            mediaRecorder.start();
+        });
+        let audioChunks = [];
+        mediaRecorder.addEventListener("dataavailable",function(event) {
+            audioChunks.push(event.data);
+        });
+
+        document.querySelector('#stop').addEventListener('click', function(){
+            mediaRecorder.stop();
+        });
+
+        mediaRecorder.addEventListener("stop", function() {
+            const audioBlob = new Blob(audioChunks, {
+                type: 'audio/wav'
+            });
+            
+            let fd = new FormData();
+            fd.append('voice', audioBlob);
+            sendVoice(fd);
+            audioChunks = [];
+        });
+    });
+
+async function sendVoice(form) {
+    let promise = await fetch(URL, {
+        method: 'POST',
+        body: form});
+    if (promise.ok) {
+        let response =  await promise.json();
+        console.log(response.action);
+        console.log('11');
+
+        let audio = document.createElement('audio');
+        audio.src = response.data;
+        audio.controls = true;
+        audio.autoplay = true;
+        document.querySelector('#messages').appendChild(audio);
+    } else {
+        let no = document.createElement('p');
+        no.innerText = 'Нет соединения с сервером';
+        document.querySelector('#messages').appendChild(no);
+    }
+}
