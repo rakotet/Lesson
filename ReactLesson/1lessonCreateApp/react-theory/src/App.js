e8243462535d2d13a42c285f5ed3fb43bb50268d
rakotet <rakotet@gmail.com>
2021-07-06 17:10:13 +0300

    lamoda

--- ReactLesson/1lessonCreateApp/react-theory/src/App.js
+++ ReactLesson/1lessonCreateApp/react-theory/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Car from './Car/Car'
+import React from 'react';
 
 // <Car name={'Ford'} year={2018}/> передаём параметры в компонент
 // <Car> <p style={{color: 'red'}}>COLOR</p> </Car> // ёще один способ передавать пареметры в компонент
-function App() {
-  const state = { // в React параметры принято передавать в объекте state (а не прям с самом компаненте писать новые значения)
+// для передачи параметров в ф-ю нужно использовать bind где первый аргумент это контекст, а второй параметры ф-и
+class App extends React.Component {
+  state = { // в React параметры принято передавать в объекте state (а не прям с самом компаненте писать новые значения) (Это объект состояния)
     cars: [
       {name: 'Ford', year: 2018},
       {name: 'Audi', year: 2016},
@@ -13,27 +15,44 @@ function App() {
     pageTitle: 'React components'
   }
 
-  const cars = state.cars
-
-  function changeTitleHandler() {
-    console.log('Clicked');
+  changeTitleHandler = (newTitle) => {
+    this.setState({ // В React менять состояния компанента можно только через метод setState
+      pageTitle: newTitle
+    })
   }
+
+  render() {
+
+    const cars = this.state.cars
 
   return (
     <div className="App">
-      <h1>{state.pageTitle}</h1>
+      <h1>{this.state.pageTitle}</h1>
 
-      <button onClick={changeTitleHandler}>Change title</button>
+      <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button> 
 
-      <Car name={cars[0].name} year={cars[0].year}>
+      <Car 
+        name={cars[0].name} 
+        year={cars[0].year}
+        onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)} // 1-й способ: ф-и других компанентов передаются через создания ф-и и передачи в неё параметров в виде ф-й этого другого компанента по ссылке (через this)
+      >
         <p style={{color: 'blue'}}>COLOR</p>
       </Car>
-      <Car name={cars[1].name} year={cars[1].year}> 
+      <Car 
+        name={cars[1].name} 
+        year={cars[1].year}
+        onChangeTitle={() => this.changeTitleHandler(cars[1].name)} // 2-й способ передачи аргументов в ф-ю компанента (через ф-ю в стрелочной ф-и), но этот способ жрет больше ресурсов браузера
+      > 
         <p style={{color: 'red'}}>COLOR</p>
       </Car>
-      <Car name={cars[2].name} year={cars[2].year}/>
+      <Car 
+        name={cars[2].name} 
+        year={cars[2].year}
+        onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+      />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- ReactLesson/1lessonCreateApp/react-theory/src/Car/Car.js
+++ ReactLesson/1lessonCreateApp/react-theory/src/Car/Car.js
@@ -18,10 +18,12 @@
 // В компоненты можно передавать параметры (props) которые заполнять в месте вызова компонента 
 //  {props.children} еще один способ передавать параметры в компонент
 // Компонент всегда должен быть обрамлен корневым <div></div>
+// <button onClick={props.onChangeTitle}>Click</button>  // так обрабатываются события в компаненте при его использовании в других компанентах (передаём ссылку на ф-ю, которая потом вызывает ф-ю компанента в который мы передаём наш компанент)
 const car = (props) => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p><strong>Year: {props.year}</strong></p> 
+        <button onClick={props.onChangeTitle}>Click</button> 
         {props.children}
     </div>
 )
