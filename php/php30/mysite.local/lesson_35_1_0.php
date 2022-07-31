5899d2b388096eed56a0d58ccac6aa3f47b03d43
rakotet <rakotet@gmail.com>
2022-07-31 20:57:19 +0300

    php dom

--- php/php30/mysite.local/lesson_35_1_0.php
+++ php/php30/mysite.local/lesson_35_1_0.php
@@ -6,5 +6,18 @@
   print_r($arr);
   echo '<br />';
 
-  
+  parse_str($arr['query'], $qyery); // ф-я парсит перданные GET параметры в строке и возвращает массив
+  print_r($qyery);
+  echo '<br />';
+
+  $qyery['a'] = 10; // изменяем, удаляем GET параметры так как нам надо
+  unset($qyery['ab']);
+  print_r($qyery);
+  echo '<br />';
+
+  $qyery = http_build_query($qyery); // ф-я из масива с GET параметрами делает обратно строку с GET параметрами учитывая все нюансы запросов и спецсимволов в строке
+  echo $qyery.'<br />';
+
+  $url = $arr['scheme'].'://'.$arr['host'].$arr['path'].'?'.$qyery; // собираем обратно url с уже измененными GET параметрами
+  echo $url;
 ?>
