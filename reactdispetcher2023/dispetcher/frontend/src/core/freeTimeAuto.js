398da154cb84960c51388d997d0eed228bd08a2d
rakotet <rakotet@gmail.com>
2023-12-13 21:26:04 +0300

    готово!! 30 мин

--- reactdispetcher2023/dispetcher/frontend/src/core/freeTimeAuto.js
+++ reactdispetcher2023/dispetcher/frontend/src/core/freeTimeAuto.js
@@ -3,9 +3,11 @@ export default function freeTimeAuto(dataItem, arrAssign) {
   let suitableAuto = []
   
     suitableAuto = arrData.map(itemData => {
-      let sTime = Number(arrAssign[1][0] + arrAssign[1][1])
-      let doTime = Number(arrAssign[2][0] + arrAssign[2][1])
-      let timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+      let sTime = arrAssign[1]
+      let doTime = arrAssign[2]
+      let timeArr = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00']
+
+      const timeArrConst = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00']
 
       let googs = true
       let item = {...itemData}
@@ -20,8 +22,8 @@ export default function freeTimeAuto(dataItem, arrAssign) {
   
           for(let key in obj) {
             for(let i = 0; i < timeArr.length; i++) {
-              if(timeArr[i] == Number(key[0] + key[1])) {
-                let ch = ((Number(obj[key][0] + obj[key][1])) - (Number(key[0] + key[1])))
+              if(timeArr[i] == key) {
+                let ch = numberTime(timeArr, i, obj[key])
                 let count = 0
   
                 for(let w = i; count < ch; w++) {
@@ -36,16 +38,16 @@ export default function freeTimeAuto(dataItem, arrAssign) {
   
           for(let q = 0; q < timeArr.length; q++) {
             if(counter == 0) {
-              if(timeArr[q] && timeArr[q] != 21) {
-                str += timeArr[q] + '-'
+              if(timeArr[q] && timeArr[q] != '21:00') {
+                str += timeArrConst[(q != 0 ? q - 1 : q)] + '-'
                 counter = 1
               }
             } else {
               if(!timeArr[q]) {
                 counter = 0
-                str += (timeArr[(q - 1)] + 1) + ';'
+                str += (timeArrConst[q]) + ';'
               } else if(q == (timeArr.length - 1)) {
-                str += 21 + ';'
+                str += '21:00' + ';'
               }
             }
           }
@@ -75,10 +77,10 @@ export default function freeTimeAuto(dataItem, arrAssign) {
             if(arrStr[z] != '') {
               if(z != index) {
                 let ar = arrStr[z].split('-')
-                strEnd += getNumber(ar[0]) + ' - ' + getNumber(ar[1]) + '; '
+                strEnd += ar[0] + ' - ' + ar[1] + '; '
               } else {
                 let ar = arrStr[z].split('-')
-                strEnd += '<span>' + getNumber(ar[0]) + ' - ' + getNumber(ar[1]) + '; </span>'
+                strEnd += '<span>' + ar[0] + ' - ' + ar[1] + '; </span>'
               }
             }
           }
@@ -105,10 +107,12 @@ export default function freeTimeAuto(dataItem, arrAssign) {
     return suitableAuto[0].freeTimeStr
 }
 
-function getNumber(num) {
-  if(num.length != 2) {
-    return `0${num}:00`
+function numberTime(arr, i, value) {
+  let count = 0
+  for(i; i < arr.length; i++) {
+    count++
+    if(arr[i] == value) break
   }
 
-  return `${num}:00`
+  return count
 }
