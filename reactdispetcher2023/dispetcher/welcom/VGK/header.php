479f4c7e905c0b38c4d3cc7c298aad84e9d2788b
rakotet <rakotet@gmail.com>
2024-07-02 15:14:47 +0300

    rab

--- reactdispetcher2023/dispetcher/welcom/VGK/header.php
+++ reactdispetcher2023/dispetcher/welcom/VGK/header.php
@@ -1,3 +1,4 @@
+<div class="header-img"></div>
 <div class="header-memo custom-margin">
    <h1>Памятка новому сотруднику</h1>
    <span>
@@ -6,6 +7,7 @@
       <div class="header-memo__line-right header-memo__show-line"></div>
    </span>
 </div>
+
 <!-- <a href="#tabs-links">
  <div class="header-arrow">
      <img src="./VGK/images/arrow-down-white1.png">
